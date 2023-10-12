98ef3fefe649b553273ab8560264b9a9aa81c048
nith-k <niden3399@gmail.com>
2023-10-12 01:09:51 -0700

    Fixed image source

--- Final-Presentation/pages/2_⚠️_Errors.py
+++ Final-Presentation/pages/2_⚠️_Errors.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,8 +6,8 @@ import scipy.stats as stats
 import streamlit as st
 
 def main():
-    st.image('/Users/nithin/Desktop/Fall_1/Comms/Hypothesis-Testing/Final_Presentation/pages/errors.jpg',
-             use_column_width=True)
+    url = 'https://raw.githubusercontent.com/bhumikasrc/Hypothesis-Testing/master/Final-Presentation/pages/errors.jpg'
+    st.image(url,use_column_width=True)
 if __name__ == '__main__':
     st.set_page_config(page_title="Errors", page_icon="⚠️",layout='wide')
     main()
